1b558ef396bff517e5eca8ee16731e79e83a63fe
Michael Herold <michael.j.herold@gmail.com>
2016-11-29 15:18:07 -0600

    Make sure to require support files in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mockle"
+
+Dir[Pathname.new(File.expand_path("../spec/support", __FILE__)).join("**/*.rb")].each { |file| require file }
